1ecb7390c117c9e44feb2eb3a9c8dd3293f51deb
Slavko Petrovic <slavko_petrovic@live.com>
2026-01-30 20:57:29 +0200

    Work on migrations

--- database/migrations/2026_01_28_195043_create_earthquakes_table.php
+++ database/migrations/2026_01_28_195043_create_earthquakes_table.php
@@ -11,13 +11,12 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('earthquakes', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->string('role');
-            $table->timestamps();
+            $table->string('external_id');
+            $table->float('magnitude');
+            $table->string('place');
+            $table->timestamp('occurred_at');
         });
     }
 
@@ -26,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('earthquakes');
     }
 };
