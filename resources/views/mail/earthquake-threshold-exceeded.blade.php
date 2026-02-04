9aaae100b48448122ed1d1bb0b4ea5ba2f838ad4
Slavko Petrovic <slavko_petrovic@live.com>
2026-02-04 06:23:48 +0100

    Simple mail template (wip)

--- resources/views/mail/earthquake-threshold-exceeded.blade.php
+++ resources/views/mail/earthquake-threshold-exceeded.blade.php
@@ -1,1 +1,28 @@
-<?php
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Earthquake events</title>
+</head>
+<body>
+    <h1>Hi {{ $user->name }}</h1>
+    <h2>Please check out list of recent earthquakes that have magnitude larger than your set treeshold {{ $user->config->magnitude_threshold }}</h2>
+    <table>
+        <tr>
+            <th>Magnitude</th>
+            <th>Location</th>
+            <th>Time</th>
+            <th>Url</th>
+        </tr>
+        @foreach ($aerthquakes as $earthquake)
+            <tr>
+                <td>{{ $earthquake->magnitude }}</td>
+                <td>{{ $earthquake->place }}</td>
+                <td>{{ $earthquake->time->format('Y-m-d H:i:s') }}</td>
+                <td>{{ $earthquake->url }}</td>
+            </tr>
+        @endforeach
+    </table>
+</body>
+</html>
